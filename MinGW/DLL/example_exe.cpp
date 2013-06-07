f40c8e77cbe8a425970e41f3f9d0837c083f00df
yushb0602 <yushb0602@gmail.com>
2013-06-07 15:41:58 +0800

    use stl in MinGW

--- MinGW/DLL/example_exe.cpp
+++ MinGW/DLL/example_exe.cpp
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include "example_dll.h"
-
+#include <vector>
 int main(void)
 {
+		//Test DLL
        hello("World");
        printf("%d\n", Double(333));
        CppFunc();
        MyClass a;
        a.func();
+		
+		//Test STL 
+		std::vector<int> v;
+		v.push_back(3);
+		v.push_back(8);
+		printf("v size:%d", v.size());
 
        return 0;
 }
